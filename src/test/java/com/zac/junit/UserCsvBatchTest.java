90c8e0116deba03ff20dd606675385cbb5c2af95
zac <zgithub@hotmail.com>
2016-09-05 20:52:09 +0800

    UT

--- src/test/java/com/zac/junit/UserCsvBatchTest.java
+++ src/test/java/com/zac/junit/UserCsvBatchTest.java
@@ -5,14 +5,20 @@ import org.junit.runner.RunWith;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import com.zac.spring_batch.user_csv.UserCsvConfig;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=UserCsvConfig.class,loader=AnnotationConfigContextLoader.class)
 public class UserCsvBatchTest {
     @Autowired
     private JobLauncherTestUtils jobLauncherTestUtils;
-
+    @Autowired
+	private UserCsvConfig userCsvConfig;
     @Test
     public void testStep() throws Exception {
         // 単独のStepの実行
